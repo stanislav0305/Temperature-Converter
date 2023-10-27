50abab8c674f5fe770b6955fefb10e7586d86249
Stanislav T <0305stas@inbox.lv>
2023-10-27 17:37:46 +0300

    Added and configurated in webpack.config.js next packages: * style-loader, * css-loader, * mini-css-extract-plugin, * sass, * sass-loader

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 import './index.html';
+import './index.scss';
 
 console.log("Hi !!!");
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 const mode = process.env.NODE_ENV || 'development';
 const devMode = mode === 'development';
@@ -23,15 +24,24 @@ module.exports = {
         clean: true,
         filename: 'index.[contenthash].js',
     },
-    plugins:[
+    plugins: [
         new HtmlWebpackPlugin({ template: path.resolve(__dirname, 'src', 'index.html') }),
+        new MiniCssExtractPlugin({ filename: 'name[contenthash].css' }),
     ],
     module: {
         rules: [
             {
                 test: /\.html$/i,
                 loader: 'html-loader',
+            },
+            {
+                test: /\.(c|sc|sa)ss$/i,
+                use: [
+                    devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'sass-loader',
+                ]
             }
         ]
-    }, 
+    },
 }
